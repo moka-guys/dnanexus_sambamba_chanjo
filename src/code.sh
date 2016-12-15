ff577c650e38459a66eb0014ea36c7ef1e3fa32e
Aled Jones <aledjones@nhs.net>
2016-12-15 10:08:49 +0000

    add comments. change the name of the output file

--- src/code.sh
+++ src/code.sh
@@ -6,42 +6,55 @@
 set -e -x -o pipefail
 
 #
-# Fetch reads
+# Fetch bed files and bam files
 #
+mark-section "Downloading inputs"
 dx download "$sambamba_bed" -o bedfile
 dx download "$bamfile"
 dx download "$bam_index" 
 
 #
-# set python path
+# add miniconda to path to ensure correct version of python is used.
 #
 PATH=/home/dnanexus/miniconda2/bin:$PATH
-ls
+
 #
 # run sambama
 #
-#bedtools sort -i bedfile > $sambamba_bed_prefix.sorted.bed
+mark-section "running sambamba"
 sambamba depth region -L bedfile -T 20 -F "mapping_quality >= 20" $bamfile_prefix.bam > $bamfile_prefix.sambamba_output.bed
 
 #
 # run chanjo
 #
+mark-section "running chanjo"
+#initiate database
 chanjo init -a 
 chanjo db setup
+# link bedfile
 chanjo  link $bamfile_prefix.sambamba_output.bed
+#load bedfile
 chanjo  load $bamfile_prefix.sambamba_output.bed
+
+# create a list of unique gene symbols
 LIST=$(cat -n $bamfile_prefix.sambamba_output.bed | awk -F '\t' '$1>1 {print $9}' | sort | uniq)
+
+#loop through list and get chanjo output
 for i in $LIST
 do
 	chanjo calculate gene $i >> chanjo_out.json
 done
-pwd
-ls
+
+mark-section "running python script to parse chanjo output"
+#python script parses the chanjo_out.json and creates a chanjo_out.csv file
 python read_chanjo.py
 
 #
 # Upload result
 #
+mark-section "uploading results"
+# make output folder
 mkdir -p ~/out/chanjo_output/
-mv chanjo_out.csv ~/out/chanjo_output/$bamfile_prefix.chanjo_out.csv
+#move and rename the output of the python script
+mv chanjo_out.csv ~/out/chanjo_output/$bamfile_prefix.chanjo_out
 dx-upload-all-outputs --parallel
